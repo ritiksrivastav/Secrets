9d342f9205326c7b9b3b42a13deb53f3f5dae600
ritiksrivastav <47292950+ritiksrivastav@users.noreply.github.com>
2020-10-12 19:29:34 +0530

    Added another piece of code in app

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://young-hamlet-64928.herokuapp.com/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -64,7 +64,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
   clientID: process.env.APP_ID,
   clientSecret: process.env.APP_SECRET,
-  callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  callbackURL: "https://young-hamlet-64928.herokuapp.com/auth/facebook/secrets"
 },
 function(accessToken, refreshToken, profile, cb) {
 
